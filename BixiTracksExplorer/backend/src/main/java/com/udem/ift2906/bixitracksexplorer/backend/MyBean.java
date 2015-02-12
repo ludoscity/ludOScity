66feffc1ebf99eed7da6ef03f47af35a0b91d229
F8Full <f8full@gmail.com>
2015-02-12 15:22:28 -0500

    Added backend

---
+++ BixiTracksExplorer/backend/src/main/java/com/udem/ift2906/bixitracksexplorer/backend/MyBean.java
@@ -0,0 +1,17 @@
+package com.udem.ift2906.bixitracksexplorer.backend;
+
+/**
+ * The object model for the data we are sending through endpoints
+ */
+public class MyBean {
+
+    private String myData;
+
+    public String getData() {
+        return myData;
+    }
+
+    public void setData(String data) {
+        myData = data;
+    }
+}
