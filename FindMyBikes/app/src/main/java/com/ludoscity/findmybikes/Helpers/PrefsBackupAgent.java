c99b229efb60c839362ee2424e939e9c227870fa
F8Full <f8full@gmail.com>
2015-10-13 16:28:26 -0400

    Backup feature.
    - Added backup agent to remotely save SharedPreferences file
    - Added privacy policy, to conforms to the Android Backup Service terms of use
    "4.10 You agree that in your use of the Service, you and your Applications will protect the privacy and legal rights of users. You must provide legally adequate privacy notice and protection for users whose data your Applications back up to the Service. Further, your Application may only use that information for the limited purpose of backing up the data to the Service unless the user has given you permission for further use. If the user has not given you permission to back up information to the Service, you may not transmit such information to the Service."
    https://developer.android.com/google/backup/terms.html

---
+++ FindMyBikes/app/src/main/java/com/ludoscity/findmybikes/Helpers/PrefsBackupAgent.java
@@ -0,0 +1,22 @@
+package com.ludoscity.findmybikes.Helpers;
+
+import android.app.backup.BackupAgentHelper;
+import android.app.backup.SharedPreferencesBackupHelper;
+
+/**
+ * Created by F8Full on 2015-10-13.
+ * Class used to interface with google backup service.
+ */
+public class PrefsBackupAgent extends BackupAgentHelper {
+
+    // A key to uniquely identify the set of backup data
+    static final String PREFS_BACKUP_KEY = "prefs";
+
+    // Allocate a helper and add it to the backup agent
+    @Override
+    public void onCreate() {
+        SharedPreferencesBackupHelper helper =
+                new SharedPreferencesBackupHelper(this, DBHelper.SHARED_PREF_FILENAME);
+        addHelper(PREFS_BACKUP_KEY, helper);
+    }
+}
