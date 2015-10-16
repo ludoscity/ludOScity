b5ce177c52ccb6cd4777f96e0d9e31d0d733ba7c
F8Full <f8full@gmail.com>
2015-10-16 01:04:43 -0400

    **Previous commit message should have read : Integrating android 6 permissions system**
    Present commit : renaming package to follow convention

--- FindMyBikes/app/src/main/java/com/ludoscity/findmybikes/RootApplication.java
+++ FindMyBikes/app/src/main/java/com/ludoscity/findmybikes/RootApplication.java
@@ -4,8 +4,8 @@ import android.app.Application;
 import android.util.Log;
 
 import com.couchbase.lite.CouchbaseLiteException;
-import com.ludoscity.findmybikes.Citybik_esAPI.Citybik_esAPI;
-import com.ludoscity.findmybikes.Helpers.DBHelper;
+import com.ludoscity.findmybikes.citybik_esAPI.Citybik_esAPI;
+import com.ludoscity.findmybikes.helpers.DBHelper;
 
 import java.io.IOException;
 
