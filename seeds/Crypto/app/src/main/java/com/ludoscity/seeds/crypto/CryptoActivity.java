d44b1d188e1433aa4a87c815f483aa0aaf8a38ea
F8Full <f8full@gmail.com>
2016-06-23 09:31:24 -0400

    Crypto seed. Initial commit.
    Output of AS 2.2 preview 3 new project wizard with empty activity. Seem to be with constraintlayout \o/ !!

---
+++ seeds/Crypto/app/src/main/java/com/ludoscity/seeds/crypto/CryptoActivity.java
@@ -0,0 +1,13 @@
+package com.ludoscity.seeds.crypto;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CryptoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crypto);
+    }
+}
