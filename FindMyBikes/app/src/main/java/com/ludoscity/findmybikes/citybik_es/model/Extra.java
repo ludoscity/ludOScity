0d8e6801101ec6793d2c3d6db4ad06322b4fd131
F8Full <f8full@gmail.com>
2017-12-02 10:07:41 -0500

    Removed FindMyBikes directory to replace it with a submodule

--- FindMyBikes/app/src/main/java/com/ludoscity/findmybikes/citybik_es/model/Extra.java
+++
@@ -1,6 +0,0 @@
-package com.ludoscity.findmybikes.citybik_es.model;
-
-public class Extra{
-    public Boolean locked;
-    public String name;
-}
--- FindMyBikes/app/src/main/java/com/ludoscity/findmybikes/citybik_es/model/NetworkStatus.java
+++
@@ -1,7 +0,0 @@
-package com.ludoscity.findmybikes.citybik_es.model;
-
-import java.util.ArrayList;
-
-public class NetworkStatus {
-    public ArrayList<Station> stations;
-}
